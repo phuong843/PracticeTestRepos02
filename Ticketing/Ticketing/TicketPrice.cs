fe412a8a6f3b3cd0ef8027bfbcb1ee5dd8e333c4
001034223 <Phuong.LUU@student.tafesa.edu.au>
2025-06-07 09:07:56 +0930

    Changed the ticket 1st price

--- Ticketing/Ticketing/TicketPrice.cs
+++ Ticketing/Ticketing/TicketPrice.cs
@@ -13,9 +13,9 @@ namespace Ticketing
         private decimal amountDue;
         private decimal mPrice;
 
-        const decimal mdecBalcony = 35.5m;
-        const decimal mdecGeneral = 28.75m;
-        const decimal mdecBox = 62.0m;
+        const decimal mdecBalcony = 45.5m;
+        const decimal mdecGeneral = 38.75m;
+        const decimal mdecBox = 72.0m;
         const decimal mdecDiscount = 5.0m; 
 
         private int Section
